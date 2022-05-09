fba217dcc0e4285bed10957b291091974dec7375
Piotr Bojczewski <piotr200310@outlook.com>
2022-05-09 19:56:54 +0200

    init

---
+++ src/main/java/com/doomedcat17/gpupriceapi/GpuPriceApiApplication.java
@@ -0,0 +1,13 @@
+package com.doomedcat17.gpupriceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpuPriceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpuPriceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doomedcat17/gpupriceapi/GpuPriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doomedcat17.gpupriceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpuPriceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
