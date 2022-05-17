855d6ea36c6ff4989846da479b6c53c30ef94702
Piotr Bojczewski <piotr200310@outlook.com>
2022-05-18 00:38:29 +0200

    refactor AppInit

--- src/main/java/com/doomedcat17/gpupriceapi/init/ApiInit.java
+++ src/main/java/com/doomedcat17/gpupriceapi/init/ApiInit.java
@@ -2,9 +2,9 @@ package com.doomedcat17.gpupriceapi.init;
 
 import com.doomedcat17.gpupriceapi.domain.GpuModel;
 import com.doomedcat17.gpupriceapi.rates.provider.CurrencyProvider;
-import com.doomedcat17.gpupriceapi.repository.GpuModelRepository;
 import com.doomedcat17.gpupriceapi.service.CurrencyService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,19 +12,21 @@ import java.util.List;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class ApiInit implements CommandLineRunner {
 
     private CurrencyService currencyService;
-    private GpuModelRepository gpuModelRepository;
     private GpuModelsLoader gpuModelsLoader;
     private CurrencyProvider currencyProvider;
 
     @Override
     public void run(String... args) throws Exception {
-        if (gpuModelRepository.count() == 0) {
-            List<GpuModel> gpuModels = ResourceLoader.loadGpuModelsFromFile();
-            gpuModelsLoader.loadGpus(gpuModels);
-        }
+        log.info("Loading Gpu Models...");
+        List<GpuModel> gpuModels = ResourceLoader.loadGpuModelsFromFile();
+        gpuModelsLoader.loadGpus(gpuModels);
+        log.info("Gpu Models loaded!");
+        log.info("Updating currencies...");
         currencyService.updateCurrencies(currencyProvider.getLatestRates());
+        log.info("Currencies updated!");
     }
 }
