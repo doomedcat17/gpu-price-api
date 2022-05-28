4de92fcd71b3f2a30689ff9c07473ed494cd6d3a
Piotr Bojczewski <piotr200310@outlook.com>
2022-05-28 20:12:23 +0200

    add GpuListingUpdateLogRepository

--- src/main/java/com/doomedcat17/gpupriceapi/repository/GpuListingUpdateLogRepository.java
+++ src/main/java/com/doomedcat17/gpupriceapi/repository/GpuListingUpdateLogRepository.java
@@ -2,8 +2,14 @@ package com.doomedcat17.gpupriceapi.repository;
 
 import com.doomedcat17.gpupriceapi.domain.log.GpuListingUpdateLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface GpuListingUpdateLogRepository extends JpaRepository<GpuListingUpdateLog, Integer> {
+
+    @Query("SELECT log FROM GpuListingUpdateLog log WHERE log.time = MAX(log.time)")
+    Optional<GpuListingUpdateLog> getLatest();
 }
