f0a358f4352b16e3288f580d9e65a0b1fd549d43
Piotr Bojczewski <piotr200310@outlook.com>
2022-06-09 23:13:47 +0200

    make SchedulingConfig based on property

--- src/main/java/com/doomedcat17/gpupriceapi/config/SchedulingConfig.java
+++ src/main/java/com/doomedcat17/gpupriceapi/config/SchedulingConfig.java
@@ -1,10 +1,13 @@
 package com.doomedcat17.gpupriceapi.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@ConditionalOnProperty(prefix = "doomedcat17.gpu-price-api.scheduling", name = "enabled", havingValue = "true", matchIfMissing = true)
+
 public class SchedulingConfig {
 
 }
