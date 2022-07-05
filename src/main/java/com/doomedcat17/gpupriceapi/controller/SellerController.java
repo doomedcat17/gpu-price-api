5efa5a2a56e49f873e165bf94d339860a237d435
Piotr Bojczewski <piotr200310@outlook.com>
2022-07-05 20:30:19 +0200

    update controllers

--- src/main/java/com/doomedcat17/gpupriceapi/controller/SellerController.java
+++ src/main/java/com/doomedcat17/gpupriceapi/controller/SellerController.java
@@ -1,7 +1,7 @@
 package com.doomedcat17.gpupriceapi.controller;
 
 import com.doomedcat17.gpupriceapi.dto.SellerDto;
-import com.doomedcat17.gpupriceapi.service.SellerDtoService;
+import com.doomedcat17.gpupriceapi.service.seller.SellerDtoService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/api/sellers")
+@RequestMapping("/api/v1/sellers")
 @AllArgsConstructor
 @CrossOrigin
 public class SellerController {
